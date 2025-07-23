5486be9ea1045102565dd48293129d914a186b55
wzeeshan <wajahatzeeshan@gmail.com>
2025-07-23 15:44:12 +0100

    edit

--- src/App.jsx
+++ src/App.jsx
@@ -1,1 +1,12 @@
-// placeholder for App.jsx
+import React from 'react';
+
+function App() {
+    return (
+        <div>
+            <h1>Welcome to WZ Vending Site</h1>
+            {/* Add your components and routes here */}
+        </div>
+    );
+}
+
+export default App;
