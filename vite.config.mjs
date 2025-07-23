a856a2f394e9f0023ca6c6a6bf98e0540cd3eee6
wzeeshan <wajahatzeeshan@gmail.com>
2025-07-23 14:19:00 +0100

    added dist folder

--- vite.config.mjs
+++ vite.config.mjs
@@ -2,5 +2,6 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
+  base: '/wz-vending-site/', // <-- Add this line
   plugins: [react()],
 });
