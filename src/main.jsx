b8e6ba3bb052a22769cc92226575c04edc0b2eec
wzeeshan <wajahatzeeshan@gmail.com>
2025-07-28 18:21:52 +0100

    edit contact

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { HashRouter, Routes, Route } from 'react-router-dom';
 
 import Layout from "./components/Layout";
 import Home from "./pages/Home";
